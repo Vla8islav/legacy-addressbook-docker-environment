fe4a3bf90a927f318629ccd44e44a1d3cc665fd8
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2019-04-16 23:12:23 +0300

    Switched back to last actually working version

--- containers/legacy-addressbook-mysql/mantis.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE bugtracker;
-USE bugtracker;
-GRANT ALL PRIVILEGES ON bugtracker.* TO 'devuser'@'localhost';
--- php/mantisbt-1.2.20/config_inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$g_hostname = 'db';
-$g_db_type = 'mysql';
-$g_database_name = 'bugtracker';
-$g_db_username = 'root';
-$g_db_password = 'root';
